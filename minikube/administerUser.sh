b2642594d8c1a2ae968def1d26ea1502eb91c0c5
jroden <joden1995@gmail.com>
2020-05-08 12:45:30 -0400

    read only role & initial administerUser commit

---
+++ minikube/administerUser.sh
@@ -0,0 +1,8 @@
+# Scenario: establish new user with X.509 cert auth strategy for k8s cluster
+# 1) generate key for user
+
+# 2) generate/approve certificate signed request for new user
+
+# 3) set authorization role for new user
+
+# 4) set credentails for user using cert & key and test permissions using 'can-i'
