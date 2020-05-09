815e4cf48cbf264c3d936fed1c59359b2d8a8e8c
James Oden <joden1995@gmail.com>
2020-05-09 12:08:57 -0400

    https://github.com/jroden/KubernetesExamples/issues/2

--- minikube/administerUser.sh
+++ minikube/administerUser.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Scenario: establish new user with X.509 cert auth strategy for k8s cluster
 # 1) generate key for user
 openssl genrsa -out test-user.key 2048
@@ -16,6 +18,7 @@ spec:
     - digital signature
     - key encipherment
     - server auth
+    - client auth
 EOF
 kubectl certificate approve test-user-csr
 
@@ -24,6 +27,7 @@ kubectl apply -f role.yml
 
 # 4) set signed cert from k8s
 kubectl get csr test-user-csr -o jsonpath='{.status.certificate}' | base64 --decode > test-user.crt
+openssl x509 -in ./test-user.crt -noout -text # verify the signed cert has expected name and organization
 
 # 5) set user credentials using signed cert & key
 kubectl config set-credentials test-user --client-certificate=./test-user.crt --client-key=./test-user.key
@@ -31,6 +35,5 @@ kubectl config set-credentials test-user --client-certificate=./test-user.crt --
 kubectl config set-context test-context --cluster=minikube --user=test-user
 
 # 6) test access
-kubectl config use-context test-user
-kubectl get pods --all-namespaces
-# kubectl auth can-i list pods --as test-user
+kubectl config use-context test-context
+kubectl get pods -n development
