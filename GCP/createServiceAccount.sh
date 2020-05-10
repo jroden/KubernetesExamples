a3722187e2d82574c44ad20a60af6ba141173f8b
James Oden <joden1995@gmail.com>
2020-05-10 17:24:04 -0400

    full automation for createServiceAccount script

--- GCP/createServiceAccount.sh
+++ GCP/createServiceAccount.sh
@@ -1,8 +1,12 @@
+#!/bin/bash
+
 # create gcp role and service account for read only kubernetes permissions
 # this service account would be assumed by other GCP services that require gke read permissions
+# note- 'srv' is abbreviation of 'service account'
 project_id=$1
 role_name=kubernetes_developer
 srv_name=kubernetes-read-only
+key_file=./key.json
 
 # read only role has permissions to list gke clusters
 gcloud iam roles create $role_name --project $project_id \
@@ -19,7 +23,11 @@ gcloud projects add-iam-policy-binding $project_id \
   --member serviceAccount:"$srv_name"@"$project_id".iam.gserviceaccount.com \
   --role projects/"$project_id"/roles/"$role_name"
 
-# pull the key file (not shown here) and activate srv for testing permissions
+# create srv key file
+gcloud iam service-accounts keys create $key_file \
+  --iam-account "$srv_name"@"$project_id".iam.gserviceaccount.com
+
+# activate srv for testing permissions
 gcloud auth activate-service-account --key-file=$key_file
 
 # get name of cluster
